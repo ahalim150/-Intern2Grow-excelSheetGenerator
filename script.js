24d80f2b7dfb0f25d7a81589bf4ac94d1b796ad1
ahalim150 <ahmedabdelhalim150@gmail.com>
2024-06-21 14:36:59 +0300

    adding export button to generate excell file exported and using sweetalert.js to display an alert when the user clicks 'generate' while the fields are empty, and when the user clicks 'export' but there is no generated table to be exported

--- script.js
+++ script.js
@@ -5,22 +5,38 @@ tableExists = false
 
 const generateTable = () => {
     let rowsNumber = parseInt(rows.value), columnsNumber = parseInt(columns.value)
+    
+    if (!rowsNumber || !columnsNumber) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Please enter the number of rows and columns!'
+        });
+        return;
+    }
+
     table.innerHTML = ""
     for(let i=0; i<rowsNumber; i++){
-        var tableRow = ""
+        var tableRow = "<tr>"
         for(let j=0; j<columnsNumber; j++){
             tableRow += `<td contenteditable></td>`
         }
+        tableRow += "</tr>"
         table.innerHTML += tableRow
     }
-    if(rowsNumber>0 && columnsNumber>0){
+    if(rowsNumber > 0 && columnsNumber > 0){
         tableExists = true
     }
 }
 
 const ExportToExcel = (type, fn, dl) => {
     if(!tableExists){
-        return
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'No table to export! Please generate a table first.'
+        });
+        return;
     }
     var elt = table
     var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" })
